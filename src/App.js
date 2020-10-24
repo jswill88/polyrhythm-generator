7f12e4e3f8e6f8aaecd1f7457f0f3573ada8848b
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-10-24 02:25:35 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import * as Tone from 'tone';
+
 import './App.css';
+
+const playSynth = async () =>{
+  await Tone.start();
+  var synth = new Tone.PolySynth(Tone.Synth).toDestination();
+  var synth2 = new Tone.PolySynth(Tone.Synth).toDestination();
+  
+  new Tone.Loop(time => {
+    synth.triggerAttackRelease("F3", '16n', time);
+  }, 6).start(0);
+  
+  new Tone.Loop(time => {
+    synth2.triggerAttackRelease("C4", '16n', time);
+  }, 7).start(0);
+  
+  Tone.Transport.bpm.value = 1000;
+  Tone.Transport.start();
+} 
+const stop = () => {
+  Tone.Transport.stop();
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='wrapper'>
+      <button id='button' onClick={playSynth}>Start</button>
+      <button id='button' onClick={stop}>Stop</button>
     </div>
   );
 }
