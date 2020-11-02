42608ca714e30a0c6fd647b2c70f1d43362fa351
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-11-02 01:14:55 -0800

    modularize notes function and animate square blinks

---
+++ src/components/Notes.js
@@ -0,0 +1,23 @@
+export default function Notes({ noteArray, rl, leftNote, rightNote, setLeftNote, setRightNote }) {
+  return (
+    <>
+      <label htmlFor="notes">Note: </label>
+      <select
+        id="notes"
+        defaultValue={rl === 'right' ? leftNote : rightNote}
+        onChange={
+          e => rl === 'right'
+            ? setLeftNote(e.target.value)
+            : setRightNote(e.target.value)
+        }>
+        {noteArray.map((note, i) =>
+          <option
+            value={note}
+            key={i}>
+            {note.slice(0, note.length - 1)}
+          </option>
+        )}
+      </select>
+    </>
+  )
+}
