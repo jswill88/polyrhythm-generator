d4c8be53ceaa9495c1de4b409858c6274e2cee4c
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-11-04 00:58:35 -0800

    add footer and style header a bit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import '../styles/footer.scss'
+
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <p>&copy; Josh Williams 2020</p>
+        <p>LinkedIn</p>
+        <p>GitHub</p>
+        <p>See Repo</p>
+
+      </div>
+    </footer>
+  )
+}
